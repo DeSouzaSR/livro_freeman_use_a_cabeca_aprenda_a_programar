d565884ed583c85cc80ea2479d262d2d2828fe69
DeSouzaSR <sandro.fisica@gmail.com>
2022-11-14 12:06:04 -0300

    Create avatar with functions, by refactoring

---
+++ avatar/avatar_functions.py
@@ -0,0 +1,15 @@
+def get_attribute(query, default):
+    question = query + "[" + default + "]? "
+    answer = input(question)
+    if(answer == ""):
+        answer = default
+    print("You chose ", answer)
+    return answer
+
+
+hair = get_attribute("What color hair? ", "brown")
+hair_length = get_attribute("What hair length? ", "short")
+eyes = get_attribute("What color eyes? ", "blue")
+gender = get_attribute("What gender? ", "female")
+has_glasses = get_attribute("Has glasses? ", "no")
+has_beard = get_attribute("Has beard? ", "no")
